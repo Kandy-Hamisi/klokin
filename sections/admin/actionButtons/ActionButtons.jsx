1dd53b00eb2cda50990a4ec33d85df5bcbaa17e9
Kandy-Hamisi <hkandy2000@gmail.com>
2024-08-13 14:22:35 +0300

    fixed paths for action buttons

--- sections/admin/actionButtons/ActionButtons.jsx
+++ sections/admin/actionButtons/ActionButtons.jsx
@@ -5,8 +5,8 @@ const ActionButtons = () => {
   return (
     <section className='max-container'>
       <div className='flex flex-col items-center'>
-        <ActionButton textContent="Assign Sites" redirect="/admin/stats" />
-        <ActionButton textContent="View Realtime Stats" redirect="/admin/siteDescription" />
+        <ActionButton textContent="Assign Sites" redirect="/admin/assign-site" />
+        <ActionButton textContent="View Realtime Stats" redirect="/admin/stats" />
       </div>
     </section>
   )
